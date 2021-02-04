67f1fa658b58f8accbce1ff81da1866276c4254d
mohafiz <mohhaf@yandex.com>
2021-02-04 11:29:38 +0200

    initial AdminLTE integration

---
+++ resources/views/livewire/admin.blade.php
@@ -0,0 +1,14 @@
+<div>
+    @extends('adminlte::page')
+
+    @section('title', 'Admin Panel')
+
+    @section('content_header')
+        <h1>Dashboard</h1>
+    @stop
+
+    @section('content')
+        <p>Welcome to this beautiful admin panel.</p>
+    @stop
+
+</div>
